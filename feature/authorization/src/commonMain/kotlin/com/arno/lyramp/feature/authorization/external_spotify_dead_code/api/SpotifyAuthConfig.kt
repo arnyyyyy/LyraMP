f8bfaa6a2ea715f736dc301b092067ede0cb5a4b
arnyyyyy <garrybobcrabsburger@gmail.com>
2026-04-15 02:17:42 +0300

    spotify api removal

--- feature/authorization/src/commonMain/kotlin/com/arno/lyramp/feature/authorization/external_spotify_dead_code/api/SpotifyAuthConfig.kt
+++
@@ -1,7 +0,0 @@
-//package com.arno.lyramp.feature.authorization.model
-//
-//expect object SpotifyAuthConfig {
-//        val CLIENT_ID: String
-//        val REDIRECT_URI: String
-//        val SCOPE: String
-//}
