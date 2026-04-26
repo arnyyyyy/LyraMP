7a0692079b30fd3d46dd4864eda8af66bf832758
arnyyyyy <garrybobcrabsburger@gmail.com>
2026-04-26 15:54:52 +0300

    listening history migration: pt1 -- lang normalizer

--- core/src/commonMain/kotlin/com/arno/lyramp/core/model/LyraLang.kt
+++ core/src/commonMain/kotlin/com/arno/lyramp/core/model/LyraLang.kt
@@ -4,6 +4,44 @@ object LyraLang {
         val SUPPORTED = setOf("en", "fr", "de", "es", "it", "hu", "ja", "zh", "he", "ar")
         val CJK: Set<String> = setOf("ja", "zh")
 
+        val LATIN_FOLD: Map<Char, String> = mapOf(
+                // lowercase
+                'à' to "a", 'á' to "a", 'â' to "a", 'ã' to "a", 'ä' to "a", 'å' to "a", 'ā' to "a", 'ă' to "a", 'ą' to "a",
+                'ç' to "c", 'ć' to "c", 'č' to "c",
+                'ď' to "d",
+                'è' to "e", 'é' to "e", 'ê' to "e", 'ë' to "e", 'ē' to "e", 'ė' to "e", 'ę' to "e",
+                'ì' to "i", 'í' to "i", 'î' to "i", 'ï' to "i", 'ī' to "i", 'į' to "i",
+                'ł' to "l",
+                'ñ' to "n", 'ń' to "n",
+                'ò' to "o", 'ó' to "o", 'ô' to "o", 'õ' to "o", 'ö' to "o", 'ø' to "o", 'ō' to "o", 'ő' to "o",
+                'ř' to "r",
+                'ś' to "s", 'š' to "s", 'ß' to "ss",
+                'ť' to "t",
+                'ù' to "u", 'ú' to "u", 'û' to "u", 'ü' to "u", 'ū' to "u", 'ů' to "u", 'ű' to "u",
+                'ý' to "y", 'ÿ' to "y",
+                'ž' to "z", 'ź' to "z", 'ż' to "z",
+                'À' to "A", 'Á' to "A", 'Â' to "A", 'Ã' to "A", 'Ä' to "A", 'Å' to "A", 'Ā' to "A", 'Ă' to "A", 'Ą' to "A",
+                'Ç' to "C", 'Ć' to "C", 'Č' to "C",
+                'Ď' to "D",
+                'È' to "E", 'É' to "E", 'Ê' to "E", 'Ë' to "E", 'Ē' to "E", 'Ė' to "E", 'Ę' to "E",
+                'Ì' to "I", 'Í' to "I", 'Î' to "I", 'Ï' to "I", 'Ī' to "I", 'Į' to "I",
+                'Ł' to "L",
+                'Ñ' to "N", 'Ń' to "N",
+                'Ò' to "O", 'Ó' to "O", 'Ô' to "O", 'Õ' to "O", 'Ö' to "O", 'Ø' to "O", 'Ō' to "O", 'Ő' to "O",
+                'Ř' to "R",
+                'Ś' to "S", 'Š' to "S",
+                'Ť' to "T",
+                'Ù' to "U", 'Ú' to "U", 'Û' to "U", 'Ü' to "U", 'Ū' to "U", 'Ů' to "U", 'Ű' to "U",
+                'Ý' to "Y", 'Ÿ' to "Y",
+                'Ž' to "Z", 'Ź' to "Z", 'Ż' to "Z",
+        )
+
+        fun String.foldLatinDiacritics(): String = buildString {
+                for (char in this@foldLatinDiacritics) {
+                        append(LATIN_FOLD[char] ?: char.toString())
+                }
+        }
+
         private val DISPLAY_NAMES: Map<String, String> = mapOf(
                 "en" to "🇬🇧 Английский",
                 "ru" to "🇷🇺 Русский",
--- feature/lyrics/src/commonMain/kotlin/com/arno/lyramp/feature/lyrics/domain/GeniusLyricsService.kt
+++ feature/lyrics/src/commonMain/kotlin/com/arno/lyramp/feature/lyrics/domain/GeniusLyricsService.kt
@@ -1,5 +1,6 @@
 package com.arno.lyramp.feature.lyrics.domain
 
+import com.arno.lyramp.core.model.LyraLang.foldLatinDiacritics
 import com.arno.lyramp.feature.lyrics.api.GeniusLyricsApi
 import com.arno.lyramp.util.Log
 
@@ -22,8 +23,8 @@ internal class GeniusLyricsService(
         }
 
         private fun buildGeniusUrl(artist: String, title: String): String {
-                val normalizedArtist = normalizeString(artist)
-                val normalizedTitle = normalizeString(title)
+                val normalizedArtist = artist.foldLatinDiacritics()
+                val normalizedTitle = title.foldLatinDiacritics()
                 val slug = "$normalizedArtist $normalizedTitle".replace("&", "and")
                         .replace(Regex("[^\\w\\s-]"), " ")
                         .trim()
@@ -31,41 +32,5 @@ internal class GeniusLyricsService(
                         .lowercase()
                         .replaceFirstChar { it.uppercaseChar() }
                 return "https://genius.com/$slug-lyrics"
-        }
-
-        private fun normalizeString(input: String): String {
-                return input
-                        .replace("à", "a")
-                        .replace("â", "a")
-                        .replace("ä", "a")
-                        .replace("ç", "c")
-                        .replace("é", "e")
-                        .replace("è", "e")
-                        .replace("ê", "e")
-                        .replace("ë", "e")
-                        .replace("î", "i")
-                        .replace("ï", "i")
-                        .replace("ô", "o")
-                        .replace("ö", "o")
-                        .replace("ù", "u")
-                        .replace("û", "u")
-                        .replace("ü", "u")
-                        .replace("ÿ", "y")
-                        .replace("À", "A")
-                        .replace("Â", "A")
-                        .replace("Ä", "A")
-                        .replace("Ç", "C")
-                        .replace("É", "E")
-                        .replace("È", "E")
-                        .replace("Ê", "E")
-                        .replace("Ë", "E")
-                        .replace("Î", "I")
-                        .replace("Ï", "I")
-                        .replace("Ô", "O")
-                        .replace("Ö", "O")
-                        .replace("Ù", "U")
-                        .replace("Û", "U")
-                        .replace("Ü", "U")
-                        .replace("Ÿ", "Y")
         }
 }
